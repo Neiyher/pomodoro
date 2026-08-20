a8f0bb5645ebba42cf3901179c0e11cf033ff02a
Neiyher <rehyien@gmail.com>
2026-08-19 19:14:57 -0500

    feat: pomodoro de ejemplo actualizado

--- src/data/Pomodoros.ts
+++ src/data/Pomodoros.ts
@@ -3,37 +3,11 @@ import type { Pomodoro } from "../types/Pomodoro"
 export const pomodoros: Pomodoro[] = [
     {
         id: 1,
-        title: "Play Piano",
-
-        workTime: 20 * 60,
-        breakTime: 4 * 60,
-        longBreakTime: 0,
-
-        targetPomodoros: 2,
-        completedPomodoros: 0,
-
-        check: false,
-    },
-    {
-        id: 2,
-        title: "Programanding",
-
-        workTime: 50 * 60,
-        breakTime: 10 * 60,
-        longBreakTime: 20 * 60,
-
-        targetPomodoros: 2,
-        completedPomodoros: 0,
-
-        check: false,
-    },
-    {
-        id: 3,
-        title: "Ejercicio",
+        title: "Estudiar",
 
         workTime: 25 * 60,
         breakTime: 5 * 60,
-        longBreakTime: 10 * 60,
+        longBreakTime: 15,
 
         targetPomodoros: 4,
         completedPomodoros: 0,
@@ -41,12 +15,25 @@ export const pomodoros: Pomodoro[] = [
         check: false,
     },
     {
-        id: 4,
-        title: "Lavar Platos",
+        id: 2,
+        title: "Gym",
+
+        workTime: 15 * 60,
+        breakTime: 2 * 60,
+        longBreakTime: 5 * 60,
+
+        targetPomodoros: 3,
+        completedPomodoros: 0,
+
+        check: false,
+    },
+    {
+        id: 3,
+        title: "Limpiar casa",
 
         workTime: 20 * 60,
-        breakTime: 0,
-        longBreakTime: 0,
+        breakTime: 0 * 60,
+        longBreakTime: 0 * 60,
 
         targetPomodoros: 1,
         completedPomodoros: 0,
